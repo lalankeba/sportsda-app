c2d520d0f98c124d12aba58fd87da857e1524a67
lalankeba <lalankeba@gmail.com>
2024-10-16 11:26:59 +0530

    updating profile view loading

--- components/profile.view.tsx
+++ components/profile.view.tsx
@@ -16,22 +16,26 @@ const ProfileView: React.FC = () => {
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       )}
-      <Row className="mb-2">
-        <Col xs={4} md={3}>
-          <label>Gender:</label>
-        </Col>
-        <Col>
-          { member?.gender }
-        </Col>
-      </Row>
-      <Row className="mb-2">
-        <Col xs={4} md={3}>
-          <label>Faculty:</label>
-        </Col>
-        <Col>
-          { member?.faculty.name }
-        </Col>
-      </Row>
+      {!isLoading && (
+        <>
+          <Row className="mb-2">
+            <Col xs={4} md={3}>
+              <label>Gender:</label>
+            </Col>
+            <Col>
+              { member?.gender }
+            </Col>
+          </Row>
+          <Row className="mb-2">
+            <Col xs={4} md={3}>
+              <label>Faculty:</label>
+            </Col>
+            <Col>
+              { member?.faculty.name }
+            </Col>
+          </Row>
+        </>
+      )}
     </>
   )
 }
